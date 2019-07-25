53043f8810dc2599e655f58d4bde7571cddbef88
DenmaSoft <denmasoft@gmail.com>
2019-07-25 11:04:41 -0400

    Fix dependencies

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'travelAgency'`, () => {
+  it(`should have as title 'hikeyApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('travelAgency');
+    expect(app.title).toEqual('hikeyApp');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to travelAgency!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to hikeyApp!');
   });
 });
